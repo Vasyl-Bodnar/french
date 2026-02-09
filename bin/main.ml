653445ed21420b2f80b57e934bbaaa270764303d
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2026-02-09 00:41:41 -0500

    Initial Commit with a SAT

---
+++ bin/main.ml
@@ -0,0 +1,30 @@
+open French.Sat
+
+let () =
+  Random.self_init () ;
+  let inputs = Array.make 10 false in
+  let err, iters =
+    anneal 1.0 100 inputs
+      [ [1; -2; 3]
+      ; [-4; 5; -6]
+      ; [7; 8; -1]
+      ; [-9; 10; 2]
+      ; [3; 5; 7]
+      ; [-8; -1; 4]
+      ; [2; -10; 6]
+      ; [9; -3; -5]
+      ; [1; 4; 7]
+      ; [-2; -6; -8]
+      ; [10; -9; 5]
+      ; [3; -4; 1]
+      ; [6; 7; 8]
+      ; [-10; 2; 4]
+      ; [5; -1; -3]
+      ; [9; 6; -2] ]
+  in
+  Printf.printf "Output: %d\nIterations: %d\nBest inputs: " err iters ;
+  Array.iter
+    (fun b ->
+      if b then print_int 1 else print_int 0 ;
+      print_string " " )
+    inputs
---
+++ lib/sat.ml
@@ -0,0 +1,39 @@
+type clauses = int list list
+
+let print_clauses (clauses : clauses) =
+  List.map (fun x -> List.map string_of_int x |> String.concat ", ") clauses
+  |> String.concat " ^ " |> print_endline
+
+let eval_err (inputs : bool array) (clauses : clauses) =
+  List.fold_left
+    (fun acc clause ->
+      if
+        List.exists
+          (fun input ->
+            (input > 0 && inputs.(input - 1))
+            || (input < 0 && not inputs.(-input - 1)) )
+          clause
+      then acc
+      else acc + 1 )
+    0 clauses
+
+let anneal (temp : float) (iterations : int) (inputs : bool array)
+    (clauses : clauses) =
+  let rec inn temp iter all_iters err =
+    if err = 0 then (err, all_iters)
+    else if temp < 0.01 then (err, all_iters)
+    else if iter = 0 then inn (temp *. 0.85) iterations all_iters err
+    else
+      let i = Random.int (Array.length inputs) in
+      inputs.(i) <- not inputs.(i) ;
+      let new_err = eval_err inputs clauses in
+      if new_err > err then
+        if
+          Float.exp (float_of_int (-(new_err - err)) /. temp) > Random.float 1.0
+        then inn temp (iter - 1) (all_iters + 1) new_err
+        else (
+          inputs.(i) <- not inputs.(i) ;
+          inn temp (iter - 1) (all_iters + 1) err )
+      else inn temp (iter - 1) (all_iters + 1) new_err
+  in
+  inn temp iterations 0 Int.max_int
