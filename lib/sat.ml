1c8d8d96d5e74fc1780c673af5d4aead481ad872
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2026-02-09 00:52:32 -0500

    Licensing and Readme

--- lib/sat.ml
+++ lib/sat.ml
@@ -1,3 +1,6 @@
+(* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. *)
 type clauses = int list list
 
 let print_clauses (clauses : clauses) =
